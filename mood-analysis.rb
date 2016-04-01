5545534fc781365bf4b1bea31005dae427a19be1
Melissa Jimison <melissajimison@gmail.com>
2016-04-01 00:33:30 -0700

    finished the last method. YAYgit add . Oh no.. I skept the 3rd...

--- mood-analysis.rb
+++ mood-analysis.rb
@@ -3,11 +3,22 @@ FEELINGS = {
   sad: %w(terrible awful horrible)
 }
 
+def strip_punctuation(words)
+  happy = 0
+  sad = 0
+  words.downcase!
+  # "03/01 I'm having a terrible horrible no good day."
+  array = words.split(" ")
+  array.map! { |word| word.delete("^a-zA-Z0-9") }
+
+end
+
 def analyze_mood(words)
   happy = 0
   sad = 0
   words.downcase!
-  words.split(" ").each do |word|
+  words = strip_punctuation(words) ##################
+  words.each do |word|
     if FEELINGS[:happy].include? word
       happy += 1
     elsif FEELINGS[:sad].include? word
@@ -35,22 +46,27 @@ puts text.map { |line| line[0,5] + " " + analyze_mood(line) }
 puts
 puts
 
-def strip_punctuation(words)
-  happy = 0
-  sad = 0
-  words.downcase!
-  # "03/01 I'm having a terrible horrible no good day."
-  array = words.split(" ")
-  array.map! { |word| word.delete("^a-zA-Z0-9") }
-  array.each do |word|
-    if FEELINGS[:happy].include? word
-      happy += 1
-    elsif FEELINGS[:sad].include? word
-      sad += 1
-    end
-  end
-  return ":-)" if happy > sad
-  return ":-(" if happy < sad
-  return ":-|"
+puts strip_punctuation(text[1])
+
+# def happy_days
+#   happy_face = 0
+#   text.lenght.times do |line|
+#     face = line[0,5] + " " + analyze_mood(line)
+#     if face.include? ":-)"
+#       happy_face += 1
+#     end
+#       break if  happy_face == 3
+#       print "It takes #{text.index} entries for 3 happy days to occur"
+#   end
+#
+# end
+# puts text[1].happy_face
+def overall_mood
+  happy_face = 0
+  sad_face = 0
+  moods = text.map { |line| analyze_mood(line) }
+  counts = Hash.new(0)
+  moods = moods.each {|mood| counts[mood] += 1}
+  moods.each { |string, number| number.max }
+  counts.key(counts.values.max)
 end
-puts text.map { |line| line[0,5] + " " + strip_punctuation(line) }
